3cf52c1c5af533572cfb72698c7f3ec3cc8c1c07
Shumbul Arifa <arifas18922@gmail.com>
2023-04-27 01:46:57 +0530

    Props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import PropExample from "./Components/PropExample";
 
 function App() {
   return (
     <div className="App">
-      Hello World!    
+      <PropExample name="Sumbul" surname="Arifa"/>
+      <PropExample name="Snowy?"/>
     </div>
   );
 }
---
+++ src/Components/PropExample.js
@@ -0,0 +1,6 @@
+const PropExample = (props) => {
+    const {surname, name}=props;
+    return <h1>Hi {name} {surname}</h1>
+}
+
+export default PropExample
